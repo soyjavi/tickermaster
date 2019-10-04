6e33db36c1b1d6c08c45d89c0a92e1f9d39124b7
rubenmoya <rmoyarodriguez@gmail.com>
2019-10-04 08:27:41 +0200

    Add missing cache middleware

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ global.connections = {};
 // -- Middlewares
 app.get('/:baseCurrency/:symbol/:group', cache, timeline);
 app.get('/:baseCurrency/latest', cache, latest);
-app.get('/convert/:baseCurrency/:amount/:symbol', converter);
+app.get('/convert/:baseCurrency/:amount/:symbol', cache, converter);
 app.get('/symbols', cache, symbols);
 app.get('/status', status);
 app.get('/backup/:key', backup);
