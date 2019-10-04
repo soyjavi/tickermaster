c6667d14783e51d94e131092d982f46b4f569c32
soyjavi <hello@soyjavi.com>
2019-10-04 09:57:03 +0700

    Improved timeline with new values: low, high & progression

--- src/services/timeline.js
+++ src/services/timeline.js
@@ -3,14 +3,17 @@ import {
 } from '../common';
 import { getHistory, median } from './modules';
 
-const { BASE_CURRENCY } = C;
+const { BASE_CURRENCY, CRYPTOS, SYMBOLS } = C;
 const GROUPS = ['H', 'D', 'W', 'M'];
+const ASSETS = [...(CRYPTOS.split(',')), 'XAU', 'XAG'];
 
-const localExchange = (rates = {}, symbol, baseCurrency) => {
+const localExchange = (rates = {}, symbol, base) => {
   let value = rates[symbol];
 
-  if (baseCurrency !== BASE_CURRENCY) {
-    const conversion = 1 / rates[baseCurrency];
+  if (ASSETS.includes(symbol)) value = 1 / value;
+
+  if (base !== BASE_CURRENCY) {
+    const conversion = ASSETS.includes(base) ? rates[base] : 1 / rates[base];
     value = (symbol === BASE_CURRENCY) ? conversion : exchange(symbol, value, conversion);
   }
 
@@ -19,16 +22,17 @@ const localExchange = (rates = {}, symbol, baseCurrency) => {
 
 export default (req, res) => {
   const { originalUrl, params: { baseCurrency, symbol, group } } = req;
-  const rates = {};
 
   // 1. Control :symbol is valid
+  if (!SYMBOLS.includes(baseCurrency) || !SYMBOLS.includes(symbol)) return ERROR.NOT_FOUND(res);
 
   // 2. Control :group is valid
-  if (!GROUPS.includes(group)) ERROR.UNKNOWN_SERVICE(res);
+  if (!GROUPS.includes(group)) return ERROR.UNKNOWN_SERVICE(res);
 
   // 3. Determine files to check
   const { now } = time();
   const history = getHistory(group === 'M' ? 12 : 1);
+  const rates = {};
 
   // 4. Process history
   let fulfilled = false;
@@ -45,14 +49,14 @@ export default (req, res) => {
           .some((hour) => {
             const value = localExchange(history[date][hour], symbol, baseCurrency);
             rates[(new Date(year, month - 1, day, hour)).toISOString()] = value;
-            fulfilled = Object.keys(rates).length >= 24;
+            fulfilled = Object.keys(rates).length >= 32;
             return fulfilled;
           });
       } else if (group === 'D') {
         const { hour } = time();
         const value = localExchange(history[date][hour], symbol, baseCurrency);
         rates[(new Date(year, month - 1, day, hour)).toISOString()] = value;
-        fulfilled = Object.keys(rates).length >= 30;
+        fulfilled = Object.keys(rates).length >= 32;
       } else if (group === 'M') {
         const values = Object.keys(history[date])
           .map((hour) => localExchange(history[date][hour], symbol, baseCurrency));
@@ -72,6 +76,13 @@ export default (req, res) => {
 
   // 6. For WEEKLY & MONTHLY group
 
+
+  // 7. determine low, high & progression
+  const values = Object.values(rates);
+  const low = Math.min(...values);
+  const high = Math.max(...values);
+  const progression = (((values[0] * 100) / [values[values.length - 1]]) - 100).toFixed(2);
+
   // 7. cache & response
   const response = {
     baseCurrency,
@@ -79,7 +90,11 @@ export default (req, res) => {
     group,
     now,
     rates,
+    low,
+    high,
+    progression,
   };
   cache.set(originalUrl, response);
-  res.json(response);
+
+  return res.json(response);
 };
