07953ec6399324939368c93278aa128ce3588218
soyjavi <hello@soyjavi.com>
2019-10-04 08:42:15 +0700

    Store available SYMBOLS in C

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv';
 
+import Store from './store';
+
 dotenv.config();
 const { BASE_CURRENCY, CACHE, CRYPTOS } = process.env || {};
+
+const store = new Store({ filename: 'symbols' });
+const SYMBOLS = Object.keys(store.read());
 
 export default {
   BASE_CURRENCY,
   CACHE,
   CRYPTOS,
+  SYMBOLS,
   URL: {
     CRYPTOCOMPARE: 'https://min-api.cryptocompare.com/data',
     METALS: 'https://metals-api.com/api',
