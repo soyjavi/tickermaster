88cf011f9d06561dfbc5f0639eeb8df3db64e736
soyjavi <hello@soyjavi.com>
2019-10-12 20:47:22 +0700

    New method within ERROR

--- src/common/error.js
+++ src/common/error.js
@@ -1,3 +1,6 @@
+import Store from './store';
+import time from './time';
+
 export default {
   FORBIDDEN: (res) => res.status(403).json({ message: 'Forbidden.' }),
 
@@ -6,6 +9,14 @@ export default {
   NOT_FOUND: (res) => res.status(404).json({ message: 'Resource not found.' }),
 
   REQUIRED_PARAMETERS: (res, parameters) => res.status(400).json({ message: `Required parameters: ${parameters}` }),
+
+  store: (message) => {
+    console.log('🔴', message);
+    const { now } = time();
+
+    const store = new Store({ filename: 'errors' });
+    store.write({ ...store.read(), [now.toISOString()]: message });
+  },
 
   UNKNOWN_SERVICE: (res) => res.status(400).json({ message: 'Unknown service.' }),
 };
